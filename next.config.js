e4c37d66c3d293ad2ba4017ce432faae1af14f2c
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-03-22 12:52:38 +0100

    connected to sanity studio

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_SANITY_DATASET: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+    NEXT_PUBLIC_SANITY_PROJECT_ID: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    NEXT_PUBLIC_SANITY_USE_CDN: process.env.NODE_ENV === "production",
+  }
 }
 
 module.exports = nextConfig
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import { sanityClient } from '../sanity';
 
-export default function Home() {
+const Home = ({properties}) => {
   return (
    <>
+   {console.log(properties)}
    </>
   )
 }
+
+export const getServerSideProps = async () => {
+  const query = '*[_type == "property"]';
+  const properties = await sanityClient.fetch(query);
+
+  if (!properties.length) {
+    return { 
+      props: {
+        properties: [],
+      },
+    }
+  } else {
+    return { 
+      props: {
+        properties,
+      },
+    }
+  }
+}
+
+export default Home;
